70365936a7f1ff9352c2455c072abf2519ca8a22
Mike Judge <mikelovesrobots@gmail.com>
2019-01-29 00:07:23 -0800

    Start filling out the TriggerActionSurge extension for when Surge is installed

---
+++ TriggerActionSurge/MoveByAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+
+public class MoveByAction : ActionBase
+{
+    public Vector3 Increment;
+    public bool IsLocal = true;
+    public float Delay = 0f;
+    public float Duration = 1f;
+    public AnimationCurve AnimationCurve = Tween.EaseInOutStrong;
+    public Tween.LoopType LoopType;
+
+    public override void Act() {
+        if (IsLocal) {
+            Tween.LocalPosition(transform, transform.localPosition, transform.localPosition + Increment, Duration, Delay, AnimationCurve, LoopType);
+        } else {
+            Tween.Position(transform, transform.position, transform.position + Increment, Duration, Delay, AnimationCurve, LoopType);
+        }
+    }
+}
---
+++ TriggerActionSurge/MoveFromAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+
+public class MoveFromAction : ActionBase
+{
+    public Vector3 Position;
+    public bool IsLocal = true;
+    public float Delay = 0f;
+    public float Duration = 1f;
+    public AnimationCurve AnimationCurve = Tween.EaseInOutStrong;
+    public Tween.LoopType LoopType;
+
+    public override void Act() {
+        if (IsLocal) {
+            Tween.LocalPosition(transform, Position, transform.localPosition, Duration, Delay, AnimationCurve, LoopType);
+        } else {
+            Tween.Position(transform, Position, transform.position, Duration, Delay, AnimationCurve, LoopType);
+        }
+    }
+}
